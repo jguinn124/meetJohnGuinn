323a59be15de4bc447ab701f894dffb5eb5a9778
John Guinn <44623156+jguinn124@users.noreply.github.com>
2021-02-09 21:09:59 -0600

    Contact Form Complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,16 @@
 import React from "react";
-import "./fonts.css";
-
+import emailjs from "emailjs-com";
+import {
+  TextField,
+  Card,
+  CardContent,
+  Typography,
+  Grid,
+  Button,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
 
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   root: {
     minWidth: 275,
   },
@@ -23,44 +25,67 @@ const useStyles = makeStyles((theme) => ({
   pos: {
     marginBottom: 12,
   },
-}));
+});
 
-export default function LandingPage() {
+export default function ContactUs() {
   const classes = useStyles();
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "contact_service",
+        "template_dzx70sl",
+        e.target,
+        "user_dfjjjh6Lgl5QtjyH5KaQ4"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    window.alert(
+      "Thank you for your Submission \n \n John will get back to you shortly!"
+    );
+  }
 
   return (
     <div>
-      <Grid container justify="center" direction="column">
-        <Grid
-          container
-          justify="center"
-          direction="row"
-          className={classes.group}
-        >
-          <Grid container justify="center">
-            <Grid
-              item
-              style={{
-                padding: "80px",
-                paddingBottom: "600px",
-              }}
-            >
-              <Card className={classes.root}>
-                <CardContent>
-                  <Typography
-                    className={classes.title}
-                    color="textSecondary"
-                    gutterBottom
-                  >
-                    While Under Construction Contact By Email
-                  </Typography>
-                  <Typography variant="h5" component="h2">
-                    jguinn124@gmail.com
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          </Grid>
+      <Grid
+        container
+        justify="center"
+        style={{ paddingTop: "40px", paddingBottom: "30vh" }}
+      >
+        <Grid item>
+          <Typography variant="h5">Contact</Typography>
+
+          <Card className={classes.root}>
+            <CardContent>
+              <form className="contact-form" onSubmit={sendEmail}>
+                <TextField
+                  id="outlined-basic"
+                  type="hidden"
+                  name="contact_number"
+                />
+                <Typography>Name</Typography>
+                <TextField id="outlined-basic" type="text" name="user_name" />
+                <Typography style={{ paddingTop: "40px" }}>Email</Typography>
+                <TextField id="outlined-basic" type="email" name="user_email" />
+                <Typography style={{ paddingTop: "40px" }}>Message</Typography>
+                <TextField
+                  style={{ paddingBottom: "40px" }}
+                  multiline
+                  name="message"
+                />
+                <br />
+                <input type="submit" value="Send" />
+              </form>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     </div>
